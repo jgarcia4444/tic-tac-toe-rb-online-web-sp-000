38fc8d372bdaffb37c5f9391d1997ecc3533db13
Jake Garcia <bballwiz4@gmail.com>
2020-02-20 00:26:00 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -59,7 +59,7 @@ def turn_count(board)
 end
 
 def current_player(board)
-  if turn_count(board).even?
+  if turn_count(board).even? || turn_count(board) == 0
     return "X"
   else
     return "O"
